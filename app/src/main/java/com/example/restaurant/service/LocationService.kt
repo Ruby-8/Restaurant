9415d1345608e0d66c9c9e4169d12102d60d9406
Ziyang Liang <lziyang004@gmail.com>
2023-10-22 14:47:37 -0700

    added LocationService

--- app/src/main/java/com/example/restaurant/service/LocationService.kt
+++ app/src/main/java/com/example/restaurant/service/LocationService.kt
@@ -1,4 +1,31 @@
 package com.example.restaurant.service
 
-class LocationService {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class LocationService(context: Context) {
+
+    private var locationManager: LocationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    private val _location = MutableLiveData<Location?>()
+    val location: LiveData<Location?> get() = _location
+
+    @SuppressLint("MissingPermission")
+    fun getCurrentLocation() {
+        val lastKnownLocation: Location? = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (lastKnownLocation != null) {
+            _location.value = lastKnownLocation
+        } else {
+            locationManager.requestSingleUpdate(
+                LocationManager.GPS_PROVIDER,
+                { location -> _location.value = location },
+                null
+            )
+        }
+    }
 }
+
