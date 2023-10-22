6a8cc0bc9a0a755250ee5147daa96a3e90316d63
Ziyang Liang <lziyang004@gmail.com>
2023-10-22 14:42:55 -0700

    added PermissionService

--- app/src/main/java/com/example/restaurant/service/PermissionService.kt
+++ app/src/main/java/com/example/restaurant/service/PermissionService.kt
@@ -1,4 +1,27 @@
 package com.example.restaurant.service
 
-class PermissionService {
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionsService(private val activity: AppCompatActivity) {
+
+    fun hasPermissions(permissions: Array<String>): Boolean {
+        return permissions.all {
+            ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    fun requestPermissions(permissions: Array<String>, requestCode: Int) {
+        ActivityCompat.requestPermissions(activity, permissions, requestCode)
+    }
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray, callback: (Boolean) -> Unit) {
+        if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            callback(true)
+        } else {
+            callback(false)
+        }
+    }
 }
